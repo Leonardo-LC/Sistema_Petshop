d8db38a2853ac801594720e8eb0caea1a6c4d21a
Leonardo-LC <leonardolopescrz@gmail.com>
2025-05-01 18:38:55 -0300

    finaliza a criação das classes Balconista, Medico, Funcionario e Pessoa

--- packages/funcionario.py
+++ packages/funcionario.py
@@ -1,10 +1,16 @@
-class Funcionario:
-    def __init__(self, cpf: str, turno: str, data_admissao: date,
+from datetime import date
+from packages.pessoa import Pessoa
+
+
+class Funcionario(Pessoa):
+    def __init__(self,nome:str, telefone:str, email:str, cpf: str, turno: str, data_admissao: date,
                  salario: float, departamento: str, nivel_acesso: int):
 
+        super().__init__(nome, telefone, email)
         self.__cpf = cpf
         self.turno = turno
         self.data_admissao = data_admissao
         self.__salario = salario
         self.departamento = departamento
         self.__nivel_acesso = nivel_acesso
+
