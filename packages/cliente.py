0264cc8debf5c3d1d89e0323b04b2d891216dd6b
Leonardo-LC <leonardolopescrz@gmail.com>
2025-05-02 21:03:36 -0300

    Iniciando todo o projeto do zero

--- packages/cliente.py
+++
@@ -1,7 +0,0 @@
-from packages.pessoa import Pessoa
-
-class Cliente(Pessoa):
-    def __init__(self, nome:str, telefone:str, email: str, animal: str, pet: str):
-        super().__init__(nome, telefone, email)
-        self.pet = pet
-
